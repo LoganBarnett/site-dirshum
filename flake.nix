0d3a50e9b99cc90154ef9fc473b6014d3147d46e
Logan Barnett <logustus@gmail.com>
2025-06-08 06:53:30 -0700

    add hugo via Nix shell, proper gitignore

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  };
+
+  outputs = { self, nixpkgs }@inputs: {
+
+    devShells.aarch64-darwin.default = let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      buildInputs = [
+        pkgs.hugo
+      ];
+    };
+
+  };
+}
